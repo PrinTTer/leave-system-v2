a0c7b8d4da058d52382223951abf6b6874888683
PrinTTer <prinn1101@gmail.com>
2025-09-17 01:43:23 +0700

    "feat/separate-role/sidebar"

--- src/app/private/layout.tsx
+++ src/app/private/layout.tsx
@@ -1,317 +1,57 @@
+// src/app/private/layout.tsx
 "use client";
-import {
-  Avatar,
-  Col,
-  Flex,
-  Layout,
-  Menu,
-  Row,
-  Typography,
-  Dropdown,
-  Space,
-} from "antd";
-import type { MenuProps } from "antd";
-import { Content, Header } from "antd/es/layout/layout";
-import Sider from "antd/es/layout/Sider";
-import { useEffect } from "react";
+
+import { useUser } from "@/app/contexts/userContext";
+import PersonnelAdminLayout from "@/app/components/sidebar/sidebar-admin";
+import PersonnelApproverLayout from "@/app/components/sidebar/sidebar-approver";
+import PersonnelUserLayout from "@/app/components/sidebar/sidebar-user";
 import { useRouter } from "next/navigation";
-import { DownOutlined } from "@ant-design/icons";
-import "@ant-design/v5-patch-for-react-19";
+import { useEffect } from "react";
 
-import * as Icons from "lucide-react";
-import { ThemWebColor } from "../utils/constants";
-import dayjs from 'dayjs';
-import 'dayjs/locale/th';
-dayjs.locale('th');
-
-export default function PersonnelAdminLayout({
+export default function PrivateLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  const { Title } = Typography;
+}) {
+  const { user } = useUser();
   const router = useRouter();
 
-  const items: MenuProps["items"] = [
-    {
-      key: "1",
-      label: "admin",
-      disabled: true,
-    },
-    {
-      type: "divider",
-    },
-    {
-      key: "2",
-      label: "บัญชีของฉัน",
-      icon: <Icons.UserRound />,
-    },
-    {
-      key: "3",
-      label: "ออกจากระบบ",
-      icon: <Icons.LogOut />,
-    },
-  ];
+  // redirect หน้าแรกตาม role ทุกครั้งที่ user.role เปลี่ยน
+  useEffect(() => {
+    if (!user) return;
 
-  useEffect(() => { }, []);
+    switch (user.role) {
+      case "admin":
+        router.push("/private/calendar");
+        break;
+      case "approver":
+      case "user":
+        router.push("/private");
+        break;
+      default:
+        router.push("/");
+    }
+  }, [user, router]);
+
+  let Sidebar;
+  switch (user?.role) {
+    case "admin":
+      Sidebar = <PersonnelAdminLayout>{children}</PersonnelAdminLayout>;
+      break;
+    case "approver":
+      Sidebar = <PersonnelApproverLayout>{children}</PersonnelApproverLayout>
+      break;
+    case "user":
+      Sidebar = <PersonnelUserLayout>{children}</PersonnelUserLayout>;
+      break;
+    default:
+      Sidebar = null; // public page หรือยังไม่ login
+  }
 
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Layout>
-        <Sider
-          width="300px"
-          style={{
-            padding: "10px",
-            overflow: "auto",
-            height: "100vh",
-            position: "sticky",
-            insetInlineStart: 0,
-            top: 0,
-            bottom: 0,
-            scrollbarWidth: "thin",
-            scrollbarGutter: "stable",
-            backgroundColor: ThemWebColor.Background,
-          }}>
-          <div style={{ marginBottom: "10px", marginTop: 10 }}>
-            <Row>
-              <Col
-                span={7}
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}>
-                <Icons.MonitorCog width={60} height={60} color="#ffffff" />
-              </Col>
-              <Col span={17}>
-                <Title
-                  style={{ marginBottom: 0, marginTop: 0, color: "#FDFEFE" }}
-                  level={3}>
-                  {"ระบบลา"}
-                </Title>
-                <Title style={{ marginTop: 0, color: "#FDFEFE" }} level={5}>
-                  {"ภายในองค์กร"}
-                </Title>
-              </Col>
-            </Row>
-          </div>
-          <Menu
-            theme="dark"
-            defaultSelectedKeys={["home"]}
-            mode="inline"
-            items={[
-              {
-                key: "home",
-                icon: <Icons.House />,
-                style: { fontSize: 16, color: "#FDFEFE" },
-                label: "ข้อมูลการลา",
-                onClick: () => {
-                  router.push(`/private`);
-                },
-              },
-              {
-                key: "calendar",
-                icon: <Icons.Calendar />,
-                style: { fontSize: 16, color: "#FDFEFE" },
-                label: "ปฏิทิน",
-                children: [
-                  {
-                    key: "viewCalendar",
-                    icon: <Icons.CalendarCheck />,
-                    style: { fontSize: 16, color: "#FDFEFE" },
-                    label: "ดูปฏิทิน",
-                    onClick: () => {
-                      router.push(`/private/calendar`);
-                    },
-                  },
-                  {
-                    key: "manageCalendar",
-                    icon: <Icons.CalendarCog />,
-                    style: { fontSize: 16, color: "#FDFEFE" },
-                    label: "จัดการปฏิทิน",
-                    onClick: () => {
-                      router.push(`/private/calendar/manage`);
-                    },
-                  },
-                ],
-              },
-              // {
-              //   key: "approval",
-              //   icon: <Icons.UserSearch />,
-              //   style: { fontSize: 16, color: "#FDFEFE" },
-              //   label: "การอนุมัติ",
-              //   onClick: () => {
-              //     router.push(`/private/approve`);
-              //   },
-              // },
-              {
-                key: "leaveManagement",
-                icon: <Icons.Settings />,
-                style: { fontSize: 16, color: "#FDFEFE" },
-                label: "การลาของบุคคลใต้บังคับบัญชา",
-                children: [
-                  {
-                    key: "genaral",
-                    icon: <Icons.UserSearch />,
-                    style: { fontSize: 16, color: "#FDFEFE" },
-                    label: "คำขออนุมัติลา",
-                    onClick: () => {
-                      router.push(`/private/approve-hitory`);
-                    },
-                  },
-                  {
-                    key: "officer",
-                    icon: <Icons.UserSearch />,
-                    style: { fontSize: 16, color: "#FDFEFE" },
-                    label: "ลาราชการ",
-                    onClick: () => {
-                      router.push(`/private/approve-hitory/Overseas`);
-                    },
-                  },
-                ],
-
-              },
-              {
-                key: "history",
-                icon: <Icons.Users />,
-                style: { fontSize: 16, color: "#FDFEFE" },
-                label: "การลาและประวัติการลา",
-                children: [
-                  {
-                    key: "approvalHistory",
-                    icon: <Icons.UserRoundCog />,
-                    style: { fontSize: 16, color: "#FDFEFE" },
-                    label: "ประวัติการอนุมัติ",
-                    onClick: () => {
-                      router.push(`/private/leave-and-history/approval-history`);
-                    },
-                  },
-                  {
-                    key: "leaveHistory",
-                    icon: <Icons.UserCog />,
-                    style: { fontSize: 16, color: "#FDFEFE" },
-                    label: "ประวัติการลา",
-                    onClick: () => {
-                      router.push(`/private/leave-and-history/leave-history`);
-                    },
-                  },
-                  {
-                    key: "leaveStatisticsMember",
-                    icon: <Icons.UserCog />,
-                    style: { fontSize: 16, color: "#FDFEFE" },
-                    label: "สถิติการลาบุคคลใต้บังคับบัญชา",
-                    onClick: () => {
-                      router.push(`/private/leave-and-history`);
-                    },
-                  },
-                  {
-                    key: "leaveStatistics",
-                    icon: <Icons.UserCog />,
-                    style: { fontSize: 16, color: "#FDFEFE" },
-                    label: "สถิติการลา",
-                    onClick: () => {
-                      router.push(`/private/leave-and-history`);
-                    },
-                  },
-                ],
-              },
-              {
-                key: "settingCalendar",
-                icon: <Icons.LaptopMinimalCheck />,
-                style: { fontSize: 16, color: "#FDFEFE" },
-                label: "การตั้งค่าการมองเห็นการลา",
-                onClick: () => {
-                  router.push(`/private/leave-visibility`);
-                },
-              },
-              {
-                key: "manageLeave",
-                icon: <Icons.UserCheck />,
-                style: { fontSize: 16, color: "#FDFEFE" },
-                label: "การจัดการลา",
-                children: [
-                  {
-                    key: "approver",
-                    icon: <Icons.UserCheck />,
-                    style: { fontSize: 16, color: "#FDFEFE" },
-                    label: "ผู้อนุม้ติ",
-                    onClick: () => {
-                      router.push(`/private/manage-approver`);
-                    },
-                  },
-                  {
-                    key: "leave",
-                    icon: <Icons.UserCheck />,
-                    style: { fontSize: 16, color: "#FDFEFE" },
-                    label: "ประเภทการลา",
-                    onClick: () => {
-                      router.push(`/private/manage-leave`);
-                    },
-                  },
-                  // {
-                  //   key: "privilege",
-                  //   icon: <Icons.UserCheck />,
-                  //   style: { fontSize: 16, color: "#FDFEFE" },
-                  //   label: "สิทธิการลา",
-                  //   onClick: () => {
-                  //     router.push(`/private/manage-privilege`);
-                  //   },
-                  // }
-                ]
-              },
-            ]}
-          />
-        </Sider>
-        <Layout>
-          <Header
-            style={{
-              textAlign: "right",
-              color: "#fff",
-              height: 70,
-              paddingInline: 48,
-              lineHeight: "64px",
-              backgroundColor: ThemWebColor.Selete,
-            }}>
-            <div
-              style={{
-                padding: "5px",
-              }}>
-              <Flex
-                style={{ marginBottom: "10px" }}
-                justify={"flex-end"}
-                align={"center"}>
-                <Avatar size={40} icon={<Icons.UserRound />} />
-                <div style={{ marginLeft: "10px" }}>
-                  <Dropdown menu={{ items }}>
-                    <a onClick={(e) => e.preventDefault()}>
-                      <Space>
-                        <div
-                          style={{
-                            color: "white",
-                          }}>{`admin`}</div>
-                        <DownOutlined
-                          style={{
-                            color: "white",
-                          }}
-                        />
-                      </Space>
-                    </a>
-                  </Dropdown>
-                </div>
-              </Flex>
-            </div>
-          </Header>
-          <Content
-            style={{
-              paddingTop: 20,
-              paddingRight: 25,
-              paddingLeft: 20,
-              marginBottom: 100,
-            }}>
-            {children}
-          </Content>
-        </Layout>
-      </Layout>
-    </Layout>
+    <div className="flex">
+      {Sidebar}
+      <main className="flex-1">{children}</main>
+    </div>
   );
 }
